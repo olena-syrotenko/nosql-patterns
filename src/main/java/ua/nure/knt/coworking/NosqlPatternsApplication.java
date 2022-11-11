b3dd1e1489a32e2cd21883282ef7fc3ff5566258
Olena Syrotenko <syrotenko.career@gmail.com>
2022-11-11 16:53:08 +0200

    init commit

---
+++ src/main/java/ua/nure/knt/coworking/NosqlPatternsApplication.java
@@ -0,0 +1,13 @@
+package ua.nure.knt.coworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosqlPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NosqlPatternsApplication.class, args);
+	}
+
+}
