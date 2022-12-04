2700a2b0b9165c035f99cdb7feb2b39c58a477e5
Olena Syrotenko <syrotenko.career@gmail.com>
2022-12-05 01:11:22 +0200

    Removed unused imports

--- src/main/java/ua/nure/knt/coworking/dao/UserDao.java
+++ src/main/java/ua/nure/knt/coworking/dao/UserDao.java
@@ -1,15 +1,9 @@
 package ua.nure.knt.coworking.dao;
 
-import com.mongodb.client.MongoCursor;
-import org.bson.Document;
 import ua.nure.knt.coworking.entity.User;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-
-import static com.mongodb.client.model.Filters.and;
-import static com.mongodb.client.model.Filters.eq;
 
 public interface UserDao {
 	User readUserByEmail(String email) throws SQLException;
